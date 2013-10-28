41d4da597e372a5631682038dc1eec0cad4fc0e5
Jon Lauters <jonathan.lauters@gmail.com>
2013-10-27 18:05:16 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+
+/* load Google API Client */
+require_once "Google_Client.php";
+require_once "contrib/Google_CalendarService.php";
+
+$apiClient = new Google_Client();
+$apiClient->setUseObjects(true);
+$service = new Google_CalendarService($apiClient);
+
+if(isset($_SESSION['oauth_access_token'])) {
+    $apiClient->setAccessToken($_SESSION['oauth_access_token']);
+} else {
+    $token = $apiClient->authenticate();
+    $_SESSION['oauth_access_token'] = $token;
+}
+
+echo <<<EOHTML
+<html>
+  <head>
+    <title>Trucking Schedule Google Calendar Test</title>
+  </head>
+
+  <body>
+    <h1>Trucking Schedule Google Calendar Test</h1>
+  </body>
+
+</html>
+EOHTML;
