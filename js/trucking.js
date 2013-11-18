cecfa33a75f6af1f068c640e54e07d7b30fb3715
Jon Lauters <jonathan.lauters@gmail.com>
2013-11-18 08:27:29 -0700

    little style some js /css

---
+++ js/trucking.js
@@ -0,0 +1,26 @@
+(function($) {
+
+    $('#starttime').datepicker();
+    $('#endtime').datepicker();
+
+    /*$('#start-time').datetimepicker({
+        timeFormat: 'HH:mm z',
+        timezoneList: [
+            { value: -500, label: 'Eastern'},
+            { value: -600, label: 'Central'},
+            { value: -700, label: 'Mountain'},
+            { value: -800, label: 'Pacific'}
+        ]
+    });*/
+
+    /* $('#end-time').datetimepicker({
+        timeFormat: 'HH:mm z',
+        timezoneList: [
+            { value: -500, label: 'Eastern'},
+            { value: -600, label: 'Central'},
+            { value: -700, label: 'Mountain'},
+            { value: -800, label: 'Pacific'}
+        ]
+    }); */
+
+})(jQuery)
